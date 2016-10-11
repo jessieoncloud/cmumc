57d777f9121ca1853ecafe25b4f719b4f5a63a45
Siyang Li <Siyang Li>
2016-10-11 18:43:43 -0400

    add data models

--- src/webproject/cmumc/models.py
+++ src/webproject/cmumc/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    created_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.BooleanField()
+    created_time = models.DateTimeField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100, default="", blank=True)
+    price = models.IntegerField()
+    status = models.CharField(max_length=10)
+    accept_list = models.ManyToManyField(User)
+
+    class Meta:
+        ordering = ['-created_time']
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    read = models.BooleanField()
+    content = models.TextField(max_length=1000)
+
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.BooleanField()
+    score = models.IntegerField()
+    review = models.TextField(max_length=400, default="", blank=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone = models.CharField(max_length=15)
+    type = models.BooleanField()
+    activation_key = models.CharField(max_length=255)
+    age = models.IntegerField(default=0, blank=True)
+    bio = models.TextField(max_length=420, default="", blank=True)
+    photo = models.ImageField(upload_to="profile_photo", blank=True)
+    venmo = models.CharField(max_length=20,default="", blank=True)
+
+
+
