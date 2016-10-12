2b5ded9b493444cd24178b2fa0f9cb4fdbe14344
Vita <vitac215@gmail.com>
2016-10-12 16:35:53 -0400

    cateogry missing

--- src/webproject/cmumc/models.py
+++ src/webproject/cmumc/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     created_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    type = models.BooleanField()
+    post_type = models.BooleanField()
+    category = models.CharField(max_length=1, choices=POST_CATEGORY_CHOICES)
     created_time = models.DateTimeField()
     date = models.DateField()
     time = models.TimeField()
@@ -20,19 +21,19 @@ class Notification(models.Model):
     read = models.BooleanField()
     content = models.TextField(max_length=1000)
 
-class Score(models.Model):
+class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    type = models.BooleanField()
+    user_type = models.BooleanField()
     score = models.IntegerField()
     review = models.TextField(max_length=400, default="", blank=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     phone = models.CharField(max_length=15)
-    type = models.BooleanField()
+    user_type = models.BooleanField()
     activation_key = models.CharField(max_length=255)
-    age = models.IntegerField(default=0, blank=True)
-    bio = models.TextField(max_length=420, default="", blank=True)
+
+    intro = models.TextField(max_length=420, default="", blank=True)
     photo = models.ImageField(upload_to="profile_photo", blank=True)
     venmo = models.CharField(max_length=20,default="", blank=True)
 
