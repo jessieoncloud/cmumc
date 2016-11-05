e758036c68d3f21d3a7a73c4fcdcd536cb3509fb
Vita <vitac215@gmail.com>
2016-11-05 16:25:51 -0400

    register, login, reset pwd

--- src/webproject/cmumc/urls.py
+++ src/webproject/cmumc/urls.py
@@ -4,12 +4,12 @@ import django.contrib.auth.views
 import cmumc.views
 
 urlpatterns = [
-    url(r'^$', cmumc.views.home, name='home'),
+    url(r'^$', cmumc.views.home, name='index'),
     url(r'^login$', django.contrib.auth.views.login, {'template_name':'cmumc/login.html'}, name='login'),
     url(r'^logout$', django.contrib.auth.views.logout_then_login, name='logout'),
     url(r'^register$', cmumc.views.register, name='register'),
     url(r'^confirm-registeration/(?P<user_name>\w+)/(?P<token>[\w.-]+)$', cmumc.views.confirm_register, name='confirm'),
-    url(r'^password_reset$', django.contrib.auth.views.password_reset, name='reset'),
+    url(r'^password_reset$', django.contrib.auth.views.password_reset, {'template_name':'cmumc/password_reset.html'}, name='reset'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         django.contrib.auth.views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^password_reset_done$', django.contrib.auth.views.password_reset_done, name='password_reset_done'),
