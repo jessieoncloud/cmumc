5bb1e582307819e86527b12df782bdff8580b2ad
Vita <vitac215@gmail.com>
2016-11-06 19:33:04 -0500

    url change setting

--- src/webproject/cmumc/static/cmumc/js/cmumc.js
+++ src/webproject/cmumc/static/cmumc/js/cmumc.js
@@ -2,16 +2,39 @@ $(document).ready(function() {
 
 	console.log("here");
 
-	// Mode 
-	$('.mode_option').click(function() {
-		var user_type = $(this).attr('value');
-		console.log("user type: "+user_type);
+	// // Mode 
+	// $('.mode_option').click(function() {
+	// 	var user_type = $(this).attr('value');
+	// 	console.log("user type: "+user_type);
 
-		// ajax sent to back end
-		$.post("/cmumc/mode", user_type)
-		.done(function(data){
-			alert();
-		});
-	})
+	// 	// ajax sent to back end
+	// 	$.post("/cmumc/mode", user_type)
+	// 	.done(function(data){
+	// 		alert("You've entered as a "+data.mode);
+	// 	});
+	// })
 
-})
+
+	// // CSRF set-up copied from Django docs
+	// function getCookie(name) {  
+	// var cookieValue = null;
+	// if (document.cookie && document.cookie != '') {
+	//     var cookies = document.cookie.split(';');
+	//     for (var i = 0; i < cookies.length; i++) {
+	//         var cookie = jQuery.trim(cookies[i]);
+	//         // Does this cookie string begin with the name we want?
+	//         if (cookie.substring(0, name.length + 1) == (name + '=')) {
+	//             cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+	//             break;
+	//         }
+	//     }
+	// }
+	// return cookieValue;
+	// }
+	// var csrftoken = getCookie('csrftoken');
+	// $.ajaxSetup({
+	// beforeSend: function(xhr, settings) {
+	//     xhr.setRequestHeader("X-CSRFToken", csrftoken);
+	// }
+	
+});
