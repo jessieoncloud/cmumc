366e63062491153ddea8e786327355d2b177e7da
Siyang Li <siyangli@andrew.cmu.edu>
2016-11-23 23:36:19 -0500

    google maps

--- src/webproject/cmumc/models.py
+++ src/webproject/cmumc/models.py
@@ -37,7 +37,7 @@ class Post(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     date = models.DateField()
     time = models.TimeField()
-    location = models.CharField(max_length=100, default="", blank=True)
+    #location = models.CharField(max_length=100, default="", blank=True)
     position = GeopositionField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     status = models.CharField(max_length=20, default="A", choices=StatusType)
