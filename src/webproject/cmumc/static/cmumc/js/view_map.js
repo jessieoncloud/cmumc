674dc582f6eb2c76c87de4c6f42b89efbe328a48
Vita <vitac215@gmail.com>
2016-11-26 22:09:21 -0500

    map display

--- src/webproject/cmumc/static/cmumc/js/view_map.js
+++ src/webproject/cmumc/static/cmumc/js/view_map.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	console.log("view_map.js");
+
 	// Get the latitude and longitude value
 	var geo = $('#view_post_location').attr('value').split(',');
 	var latitude = parseFloat(geo[0]);
@@ -7,43 +7,19 @@ $(document).ready(function() {
 
 	var map;
 
-	function initialize() {
-		var mapDiv = $('#map-canvas');
-		map = new google.maps.Map(mapDiv, {
-		  center: new google.maps.LatLng(latitude, longitude),
-		  zoom: 12,
-		  mapTypeId: google.maps.MapTypeId.ROADMAP
+	function initMap() {
+		var center = {lat: latitude, lng: longitude};
+		console.log(center);
+		var map = new google.maps.Map(document.getElementById('map-canvas'), {
+		  zoom: 17,
+		  center: center
 		});
-
-		google.maps.event.addListenerOnce(map, 'tilesloaded');
+		var marker = new google.maps.Marker({
+		  position: center,
+		  map: map
+		});
 	}
 
-	// function addMarkers() {
-	// 	var point = new google.maps.LatLng({{mark.position.latitude}},{{mark.position.longitude}});
-	// 	    var image = '{{ STATIC_PREFIX }}'+ 'checkmark.png';
-	// 	    var marker = new google.maps.Marker({
-	// 	    position: point,
-	// 	    map: map,
-	// 	    icon: image, 
-	// 	    url: 'http://172.16.0.101:8882/zone/' + {{mark.id}},
-	// 	   title: '{{ mark.id }}',
-	// 	});
-	// 	     marker['infowindow']  = new google.maps.InfoWindow({
-	// 	             content: "<h1>{{mark.name}}</h1> <br> {{ mark.name }} <p> <a href=\"http:\/\/172.16.0.101:8882\/zone\/{{ mark.id }}\"> {{ mark.name }}</a>",
-	// 	});
-	// 	    google.maps.event.addListener(marker, 'click', function() {
-	// 	        //window.location.href = this.url;
-	// 	         this['infowindow'].open(map, this);
-	// 	    });
-	// 	   google.maps.event.addListener(marker, 'mouseover', function() {
-	// 	        // this['infowindow'].open(map, this);
-	// 	            });
-	// 	   google.maps.event.addListener(marker, 'mouseout', function() {
-	// 	        // this['infowindow'].close(map, this);
-
-	// 	    });
-	// }
-
-	google.maps.event.addDomListener(window, 'load', initialize);
+	initMap();
 });
 
