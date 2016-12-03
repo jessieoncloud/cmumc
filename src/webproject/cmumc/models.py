eeb744817000e4bf965aad2507ef8c563f50095a
Siyang Li <siyangli@andrew.cmu.edu>
2016-12-03 18:59:36 -0500

    add time to rating

--- src/webproject/cmumc/models.py
+++ src/webproject/cmumc/models.py
@@ -86,10 +86,14 @@ class Notification(models.Model):
 
 class Rating(models.Model):
     created_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     rated_user_type = models.CharField(max_length=10, choices=UserType)
     score = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)
     review = models.TextField(max_length=400, default="", blank=True)
+
+    class Meta:
+        ordering = ['-created_time']
 
 class Profile(models.Model):
     YEAR_IN_SCHOOL_CHOICES = (
